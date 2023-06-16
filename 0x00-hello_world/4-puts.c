0b882994e1bc827057e0f6c171b195f334b62555
oswealth <osbertjaywealth@gmail.com>
2023-06-15 19:56:07 -0700

    puts, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - print a line of code using puts
- * return:0 if exited properly, non-zero otherwise
- */
-int main(void)
+  *  main - prints a given phrase
+  *  Return: 0 if exited properly, non-zero otherwise
+  */
+int  main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
