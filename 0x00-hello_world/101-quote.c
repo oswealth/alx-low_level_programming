833e654f5d03e0a86378761fb988cf7a2340706f
oswealth <osbertjaywealth@gmail.com>
2023-06-15 21:06:25 -0700

    Dora Korpar

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
  * followed by a new line, to the standard error.
  * Return: Always 0 (Success)
  */
-  
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
