67c6b4fb497c11b20b7970c383dbc572d8954a52
oswealth <osbertjaywealth@gmail.com>
2023-06-20 09:34:53 -0700

    Write a program that prints

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Print "_putchar" followed by a new line
+ * Description: You are not allowed to include standard libraries
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar('\n');
+	return (0);
+}
