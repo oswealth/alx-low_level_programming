3e2a79915b98f6e9a1a4e2fc4d4999ff299607e3
oswealth <osbertjaywealth@gmail.com>
2023-06-22 10:14:26 -0700

    function that checks for uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
+
 /**
- * isupper - uppercase letters
+ * _isupper - uppercase letters
  * @c: char to check
  *
  * Return: 0 or 1
  */
+
 
 int _isupper(int c)
 {
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -10,3 +14,7 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+
+
+
+#endif
