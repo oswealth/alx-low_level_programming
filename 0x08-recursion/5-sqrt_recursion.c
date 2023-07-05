75fa3510851c0b792ad10a16d15a3d10560549b2
oswealth <osbertjaywealth@gmail.com>
2023-07-05 11:29:07 -0700

    Write a function that returns the natural square root of a number, If n does not have a natural square root, the function should return -1

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - return the natural square root of a number
+ * @n: int number
+ *
+ * Return: If no natural square root, return -1. Else return natural
+ * square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	int sqrt_fun(int n, int sq);
+
+	int sq = 1;
+
+	return (sqrt_fun(n, sq));
+}
+
+/**
+ * sqrt_fun - helper function to solve _sqrt_recursion
+ * @sq: number to determine if square root
+ * @n:  int number
+ *
+ * Return: square root if natural square root, or -1 if none found
+ */
+
+int sqrt_fun(int n, int sq)
+{
+	if (n == sq * sq)
+	{
+		return (sq);
+	}
+	else if (sq < n)
+	{
+		return (sqrt_fun(n, ++sq));
+	}
+	else
+	{
+		return (-1);
+	}
+}
