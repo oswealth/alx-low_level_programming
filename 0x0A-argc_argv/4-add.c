83fe3d5a1d1277d979a728d2459c48839c81104d
oswealth <osbertjaywealth@gmail.com>
2023-07-07 21:59:26 -0700

    Write a program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,6 +3,33 @@
 #include <ctype.h>
 #include <string.h>
 
+/**
+ * main - Prints the addition of positive numbers,
+ *        followed by a new line.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: If one of the numbers contains symbols that are non-digits - 1.
+ *         Otherwise - 0.
+ */
+
+int check_num(char *str)
+{
+	/*Declaring variables*/
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str)) /*count string*/
+		{
+			if (!isdigit(str[count])) /*check if str there are digit*/
+			{
+				return (0);
+			}
+			count++;
+		}
+	return (1);
+}
+
 
 /**
  * main - Prints the addition of positive numbers,
@@ -16,24 +43,28 @@
 
 int main(int argc, char *argv[])
 {
-	int sum, i, j;
+	/*Declaring variables*/
+	int count;
+	int str_to_int;
+	int sum = 0;
 
-	sum = 0;
-i	if (argc != 1)
+	count = 1;
+	while (count < argc) /*Goes through the whole array*/
 	{
-		for (i = 1; i < argc; i++)
+		if (check_num(argv[count]))
 		{
-			for (j = 0; j < (int)strlen(argv[i]); j++)
-			{
-				if (!isdigit(argv[i][j]))
-				{
-					printf("Error\n");
-					return (1);
-				}
-			}
-			sum += atoi(argv[i]);
+			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+			sum += str_to_int;
 		}
+			/*Condition if one of the number contains symbols that are not digits*/
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+			count++;
 	}
-	printf("%d\n", sum);
+	printf("%d\n", sum); /*print sum*/
+
 	return (0);
 }
