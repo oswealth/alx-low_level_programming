158623b8c02ed1d3610faeb512b4b8d1548fca39
oswealth <osbertjaywealth@gmail.com>
2023-07-07 20:17:35 -0700

    Write a program that adds positive numbers If one of the numbers contains symbols that are non-digits - 1 otherwise o

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+
+/**
+ * main - Prints the addition of positive numbers,
+ *        followed by a new line.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: If one of the numbers contains symbols that are non-digits - 1.
+ *         Otherwise - 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum, i, j;
+
+	sum = 0;
+i	if (argc != 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; j < (int)strlen(argv[i]); j++)
+			{
+				if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
