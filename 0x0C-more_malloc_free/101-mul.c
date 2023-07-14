f57e9609cec72a0b2ead6c9e8149570481a6f0a5
oswealth <osbertjaywealth@gmail.com>
2023-07-13 22:07:50 -0700

    Write a program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -61,10 +61,10 @@ int main(int argc, char *argv[])
 {
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
 	s1 = argv[1], s2 = argv[2];
 
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-
 		errors();
 
 	len1 = _strlen(s1);
