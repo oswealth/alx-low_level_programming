cc499c14340b550dedb531777a999c4ed52ee23b
oswealth <osbertjaywealth@gmail.com>
2023-08-07 17:53:58 -0700

    Write a function that reads a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+int _putchar(char c);
+int _strlen(char *str);
+
+#endif
