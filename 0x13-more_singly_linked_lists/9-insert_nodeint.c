24a3c176b1c364d09d40e014748d0b262bc60334
oswealth <osbertjaywealth@gmail.com>
2023-08-09 17:49:20 -0700

    Hot fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,8 +10,8 @@
  * Return: the address of the new node, or NULL if it failed
  */
 
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
-{
+	listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+	{
 	listint_t *new_node;
 	listint_t *temp = *head;
 	unsigned int x;
@@ -19,29 +19,25 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node = malloc(sizeof(listint_t));
 
 	if (!new_node || head)
-	{
-		return (NULL);
-		new_node->n = n;
-		new_node->next = NULL;
-	}
+	return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
 	if (idx == 0)
 	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 	}
 	for (x = 0; temp && x < idx; x++)
 	{
-		if (x == idx - 1)
-		{
-			new_node->next = temp->next;
-			temp->next = new_node;
-			return (new_node);
-		}
-		else
-		{
-			temp = temp->next;
-		}
+	if (x == idx - 1)
+	{
+	new_node->next = temp->next;
+	temp->next = new_node;
+	return (new_node);
+	}
+	else
+	temp = temp->next;
 	}
 	return (NULL);
-}
+	}
