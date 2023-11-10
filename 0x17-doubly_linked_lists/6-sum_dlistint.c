b527d8b7e2115a6a61425d746a0e292db1f0a791
oswealth <osbertjaywealth@gmail.com>
2023-11-10 01:49:32 -0800

    Solution to Task 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - returns the sum of all the data (n) of a dlistint_t linked list
+ * sum_dlistint - returns the sum of all the data (n) of dlistint_t linked list
  * @head: pointer to the head of the list
  * Return: the sum of the data, or 0 if the list is empty
  */
@@ -10,6 +10,7 @@ int sum_dlistint(dlistint_t *head)
 {
 	const dlistint_t *tmp;
 	int sum = 0;
+
 	tmp = head;
 
 	while (tmp != NULL)
