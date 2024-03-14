fe4f35dcdf0cabc496dbfac290e78a32acf788cb
oswealth <osbertjaywealth@gmail.com>
2024-03-13 18:59:36 -0700

    Exponential search

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,81 @@
+#include "search_algos.h"
+
+/**
+ * min - Returns the minimum of two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: The smaller of a and b
+ */
+int min(int a, int b)
+{
+	return (a < b ? a : b);
+}
+
+/**
+ * binary_search - Helper function to perform binary search
+ * @array: Pointer to the first element of the array to search in
+ * @left: The starting index of the subarray to search
+ * @right: The ending index of the subarray to search
+ * @value: Value to search for
+ *
+ * Return: Index where value is located, or -1 if value is not present
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t mid;
+	size_t i;
+
+	while (left <= right)
+	{
+		mid = left + (right - left) / 2;
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+			if (i < right)
+				printf(", ");
+		}
+		printf("\n");
+
+		if (array[mid] == value)
+			return (mid);
+		if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+	return (-1);
+}
+
+/**
+ * exponential_search - Searches for a value in a sorted array of
+ * integers using the Exponential search algorithm
+ * @array: Pointer to the first element of the array to search in
+ * @size: Number of elements in the array
+ * @value: Value to search for
+ *
+ * Return: First index where value is located, or -1 if
+ * value is not present or array is NULL
+ */
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t bound = 1;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	if (array[0] == value)
+		return (0);
+
+	while (bound < size && array[bound] <= value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", bound,
+				array[bound]);
+		bound *= 2;
+	}
+	return (binary_search(array + bound / 2, min(bound,
+					size - 1) - bound / 2 + 1, value));
+}
